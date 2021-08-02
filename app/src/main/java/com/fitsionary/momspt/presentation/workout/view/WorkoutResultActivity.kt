ca7846a6fdd9ff28fd60a6071a2e122ec4bff437
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 12:51:49 +0900

    [Feat] Add WorkoutResultActivity with WorkoutResultViewModel

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/workout/view/WorkoutResultActivity.kt
@@ -0,0 +1,26 @@
+package com.fitsionary.momspt.presentation.workout.view
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.fitsionary.momspt.R
+import com.fitsionary.momspt.databinding.ActivityWorkoutResultBinding
+import com.fitsionary.momspt.presentation.base.BaseActivity
+import com.fitsionary.momspt.presentation.workout.viewmodel.WorkoutResultViewModel
+
+class WorkoutResultActivity
+    : BaseActivity<ActivityWorkoutResultBinding, WorkoutResultViewModel>(R.layout.activity_workout_result) {
+    override val viewModel: WorkoutResultViewModel by lazy {
+        ViewModelProvider(this).get(WorkoutResultViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val resultCumulativeScore = intent.getIntExtra("result_cumulative_score",0)
+        binding.tvResultCumulativeScore.text = "${resultCumulativeScore}점"
+
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+    }
+}
