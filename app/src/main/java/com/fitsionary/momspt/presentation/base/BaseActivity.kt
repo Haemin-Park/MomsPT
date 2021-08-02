63c481d169e239995659df22a9e4b1b008c7d423
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 13:12:35 +0900

    [Feat] Implement LoadingFragment

--- app/src/main/java/com/fitsionary/momspt/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/base/BaseActivity.kt
@@ -1,16 +1,20 @@
 package com.fitsionary.momspt.presentation.base
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.fitsionary.momspt.presentation.loading.view.LoadingFragment
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes private val layoutResId: Int) :
     AppCompatActivity() {
     protected lateinit var binding: B
     protected abstract val viewModel: VM
+    private var loadingFragment: LoadingFragment? = null
+    protected val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,4 +22,39 @@ abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes 
         binding.lifecycleOwner = this
     }
 
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+    fun showLoading() {
+        this.runOnUiThread {
+            if (loadingFragment != null || (loadingFragment?.dialog)?.isShowing == true) {
+                return@runOnUiThread
+            } else {
+                loadingFragment = LoadingFragment.newInstance()
+                loadingFragment?.show(supportFragmentManager, "LOADING_FRAGMENT")
+            }
+        }
+
+        this.runOnUiThread {
+            if (loadingFragment != null || (loadingFragment?.dialog)?.isShowing == true) {
+                return@runOnUiThread
+            } else {
+                loadingFragment = LoadingFragment.newInstance()
+                loadingFragment?.show(supportFragmentManager, "LOADING_FRAGMENT")
+            }
+        }
+    }
+
+    fun hideLoading() {
+        this.runOnUiThread {
+            (supportFragmentManager.findFragmentByTag("LOADING_FRAGMENT") as? LoadingFragment)?.dismiss()
+            loadingFragment = null
+        }
+    }
+
+    protected fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/fitsionary/momspt/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/base/BaseViewModel.kt
@@ -3,9 +3,11 @@ package com.fitsionary.momspt.presentation.base
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 abstract class BaseViewModel : ViewModel() {
     private val compositeDisposable = CompositeDisposable()
+    val isLoading: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
 
     fun addDisposable(disposable: Disposable) {
         compositeDisposable.add(disposable)
