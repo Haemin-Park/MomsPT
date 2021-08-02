436a755ee7becd8d047434feec9965a83d43d2b3
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 13:02:05 +0900

    [Feat] Implement workout recyclerView

--- app/src/main/java/com/fitsionary/momspt/presentation/workout/view/WorkoutFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/workout/view/WorkoutFragment.kt
@@ -1,14 +1,52 @@
 package com.fitsionary.momspt.presentation.workout.view
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import com.fitsionary.momspt.BR
 import com.fitsionary.momspt.R
+import com.fitsionary.momspt.data.api.response.WorkoutItem
 import com.fitsionary.momspt.databinding.FragmentWorkoutBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
+import com.fitsionary.momspt.presentation.base.BaseRecyclerView
+import com.fitsionary.momspt.presentation.main.view.MainActivity
 import com.fitsionary.momspt.presentation.workout.viewmodel.WorkoutViewModel
+import com.fitsionary.momspt.util.listener.OnItemClickListener
 
 class WorkoutFragment :
     BaseFragment<FragmentWorkoutBinding, WorkoutViewModel>(R.layout.fragment_workout) {
     override val viewModel: WorkoutViewModel by lazy {
         ViewModelProvider(this).get(WorkoutViewModel::class.java)
+    }
+
+    private lateinit var currentActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MainActivity)
+            currentActivity = activity as MainActivity
+    }
+
+    private val routineAdapter = object : BaseRecyclerView<FragmentWorkoutBinding, WorkoutItem>(
+        layoutResId = R.layout.item_workout_fragment_workout,
+        bindingVariableItemId = BR.WorkoutItem,
+        bindingVariableListenerId = BR.Listener
+    ) {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            vm = viewModel
+            rvWorkout.adapter = routineAdapter
+        }
+
+        routineAdapter.onItemClickListener = object : OnItemClickListener<WorkoutItem> {
+            override fun onClick(item: WorkoutItem) {
+                val intent = Intent(currentActivity, WorkoutStartActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/fitsionary/momspt/presentation/workout/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/workout/viewmodel/WorkoutViewModel.kt
@@ -1,6 +1,14 @@
 package com.fitsionary.momspt.presentation.workout.viewmodel
 
+import androidx.lifecycle.MutableLiveData
+import com.fitsionary.momspt.data.api.response.WorkoutItem
+import com.fitsionary.momspt.data.api.response.getSampleRoutineItem
 import com.fitsionary.momspt.presentation.base.BaseViewModel
 
 class WorkoutViewModel : BaseViewModel() {
+    val workoutList = MutableLiveData<List<WorkoutItem>>()
+
+    init {
+        workoutList.postValue(getSampleRoutineItem())
+    }
 }
