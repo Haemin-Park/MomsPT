507141cabe9b43730f7fa8620f15fcb87d1fb4c9
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 13:11:08 +0900

    [Feat] Add api

--- app/src/main/java/com/fitsionary/momspt/network/Api.kt
+++ app/src/main/java/com/fitsionary/momspt/network/Api.kt
@@ -3,10 +3,19 @@ package com.fitsionary.momspt.network
 import com.fitsionary.momspt.data.api.request.PoseRequest
 import com.fitsionary.momspt.data.api.response.ScoreResponse
 import io.reactivex.rxjava3.core.Single
+import okhttp3.MultipartBody
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface Api {
     @POST("/pt/input")
     fun sendPose(@Body body: PoseRequest): Single<ScoreResponse>
+
+    @Multipart
+    @POST("/upload")
+    fun sendVideo(
+        @Part file: MultipartBody.Part
+    ): Single<String>
 }
