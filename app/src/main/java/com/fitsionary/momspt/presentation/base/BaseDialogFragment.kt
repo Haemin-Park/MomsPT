98123cf62bc35473b10baea6e812102e66d706ea
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 16:24:16 +0900

    [Feat] Implement BaseDialogFragment

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/base/BaseDialogFragment.kt
@@ -0,0 +1,30 @@
+package com.fitsionary.momspt.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+abstract class BaseDialogFragment<B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes private val layoutResId: Int) :
+    DialogFragment() {
+    protected lateinit var binding: B
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+    }
+}
