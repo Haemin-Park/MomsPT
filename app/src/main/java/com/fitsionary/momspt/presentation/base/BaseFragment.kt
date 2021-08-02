d99e8829a29b703ed9e781238c249660e7d2ee26
박해민 <phm9368@sookmyung.ac.kr>
2021-08-02 16:29:00 +0900

    [Feat] Implement loading method

--- app/src/main/java/com/fitsionary/momspt/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/base/BaseFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.fitsionary.momspt.presentation.loading.view.LoadingDialogFragment
+import com.fitsionary.momspt.presentation.loading.view.LoadingDialogFragment.Companion.LOADING_DIALOG_FRAGMENT_TAG
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class BaseFragment<B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes private val layoutResId: Int) :
     Fragment() {
     protected lateinit var binding: B
     protected abstract val viewModel: VM
+    private var loadingDialogFragment: LoadingDialogFragment? = null
+    protected val compositeDisposable = CompositeDisposable()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,5 +32,40 @@ abstract class BaseFragment<B : ViewDataBinding, VM : BaseViewModel>(@LayoutRes 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+    protected fun onBackPressed() {
+        if (parentFragmentManager.backStackEntryCount > 0) {
+            parentFragmentManager.popBackStack()
+        } else {
+            activity?.finish()
+        }
+    }
+
+    fun showLoading() {
+        activity?.runOnUiThread {
+            if (loadingDialogFragment != null || (loadingDialogFragment?.dialog)?.isShowing == true) {
+                return@runOnUiThread
+            } else {
+                loadingDialogFragment = LoadingDialogFragment.newInstance()
+                loadingDialogFragment?.show(childFragmentManager, LOADING_DIALOG_FRAGMENT_TAG)
+            }
+        }
+    }
+
+    fun hideLoading() {
+        activity?.runOnUiThread {
+            (childFragmentManager.findFragmentByTag(LOADING_DIALOG_FRAGMENT_TAG) as? LoadingDialogFragment)?.dismiss()
+            loadingDialogFragment = null
+        }
+    }
+
+    protected fun showToast(msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 }
