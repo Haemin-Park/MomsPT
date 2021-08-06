a8b3fb0c4a15e595b41931ec542cc54f41bcd3d6
박해민 <phm9368@sookmyung.ac.kr>
2021-08-06 12:35:07 +0900

    [Feat] Implement test network module

--- app/src/main/java/com/fitsionary/momspt/network/NetworkService.kt
+++ app/src/main/java/com/fitsionary/momspt/network/NetworkService.kt
@@ -1,11 +1,13 @@
 package com.fitsionary.momspt.network
 
+import com.fitsionary.momspt.BASE_URL1
 import com.fitsionary.momspt.BASE_URL2
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 
 object NetworkService {
@@ -16,9 +18,9 @@ object NetworkService {
                 .build()
             it.proceed(request)
         }
-        .connectTimeout(100, TimeUnit.SECONDS)
-        .readTimeout(100, TimeUnit.SECONDS)
-        .writeTimeout(100, TimeUnit.SECONDS)
+        .connectTimeout(500, TimeUnit.SECONDS)
+        .readTimeout(500, TimeUnit.SECONDS)
+        .writeTimeout(500, TimeUnit.SECONDS)
         .build()
 
     private val gson = GsonBuilder().setLenient().create()
@@ -27,8 +29,17 @@ object NetworkService {
         .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create(gson))
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .baseUrl(BASE_URL2)
+        .baseUrl(BASE_URL1)
         .build()
 
     val api: Api = retrofit.create(Api::class.java)
+
+    private val retrofit2 = Retrofit.Builder()
+        .client(okHttpClient)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .baseUrl(BASE_URL2)
+        .build()
+
+    val api2: Api = retrofit.create(Api::class.java)
 }
