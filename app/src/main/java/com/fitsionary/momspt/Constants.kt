daab51026a7dea6b481fa07a3a13400aebc4d52b
박해민 <phm9368@sookmyung.ac.kr>
2021-08-06 12:34:06 +0900

    [Chore] Fix base url

--- app/src/main/java/com/fitsionary/momspt/Constants.kt
+++ app/src/main/java/com/fitsionary/momspt/Constants.kt
@@ -1,4 +1,4 @@
 package com.fitsionary.momspt
 
-const val BASE_URL = "http://3.37.163.248:8080"
-const val BASE_URL2 = "http://175.209.7.227:4500"
+const val BASE_URL1 = "http://3.37.163.248:8080"
+const val BASE_URL2 = "http://125.129.117.140:4500"
