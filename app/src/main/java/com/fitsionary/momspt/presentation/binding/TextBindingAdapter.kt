0a0fe35ad0c7cbc67f8680485ab706cd91f0e952
박해민 <phm9368@sookmyung.ac.kr>
2021-08-09 23:50:14 +0900

    [Feat] Implement TextBindingAdapter

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
@@ -0,0 +1,49 @@
+package com.fitsionary.momspt.presentation.binding
+
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.fitsionary.momspt.R
+
+@BindingAdapter("score_gradient")
+fun setScoreGradient(textView: TextView, score: Int) {
+    val paint = textView.paint
+    val width = paint.measureText(textView.text.toString())
+
+    when (score) {
+        in 0..59 -> {
+            textView.setTextColor(textView.resources.getColor(R.color.materialBlue))
+            textView.paint.shader = null
+        }
+        in 60..89 -> {
+            textView.setTextColor(textView.resources.getColor(R.color.materialPink))
+            textView.paint.shader = null
+        }
+        else -> {
+            val textShader = LinearGradient(
+                0f, 0f, width, textView.textSize, intArrayOf(
+                    textView.resources.getColor(R.color.materialPink),
+                    textView.resources.getColor(R.color.materialBlue),
+                ), null, Shader.TileMode.CLAMP
+            )
+            textView.paint.shader = textShader
+        }
+    }
+}
+
+@BindingAdapter("score_comment")
+fun setScoreComment(textView: TextView, score: Int) {
+    val comment = when (score) {
+        in 0..59 -> {
+            "Bad"
+        }
+        in 60..89 -> {
+            "Great!"
+        }
+        else -> {
+            "Perfect!"
+        }
+    }
+    textView.text = comment
+}
