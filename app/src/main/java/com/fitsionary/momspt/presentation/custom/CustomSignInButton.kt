4f925e8250adc6ee340b8206faba6f25355219a4
박해민 <phm9368@sookmyung.ac.kr>
2021-09-06 19:39:47 +0900

    [Feat] Implement custom sign in button

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomSignInButton.kt
@@ -0,0 +1,50 @@
+package com.fitsionary.momspt.presentation.custom
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import androidx.databinding.DataBindingUtil
+import com.fitsionary.momspt.R
+import com.fitsionary.momspt.databinding.CustomSignInButtonBinding
+
+
+class CustomSignInButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        val binding: CustomSignInButtonBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.custom_sign_in_button,
+            this,
+            true
+        )
+
+        context.withStyledAttributes(attrs, R.styleable.SignInButton) {
+            binding.layoutCustomSignIn.backgroundTintList =
+                ColorStateList.valueOf(
+                    getColor(
+                        R.styleable.SignInButton_bgColor,
+                        Color.BLACK
+                    )
+                )
+            binding.tvCustomSignIn.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                getDrawable(R.styleable.SignInButton_symbolImg),
+                null,
+                null,
+                null
+            )
+            binding.tvCustomSignIn.text = getString(R.styleable.SignInButton_text)
+            binding.tvCustomSignIn.setTextColor(
+                getColor(
+                    R.styleable.SignInButton_textColor,
+                    Color.WHITE
+                )
+            )
+        }
+    }
+}
