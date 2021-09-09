9b1134bdf9058e2b3ec2243d452a76c0f52b9aaf
박해민 <phm9368@sookmyung.ac.kr>
2021-09-09 15:07:33 +0900

    [Feat] Implement additional attrs

--- app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomSignUpEditText.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomSignUpEditText.kt
@@ -2,11 +2,13 @@ package com.fitsionary.momspt.presentation.custom
 
 import android.content.Context
 import android.graphics.Color
+import android.text.InputFilter
+import android.text.InputFilter.LengthFilter
+import android.text.InputType
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.withStyledAttributes
-import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.CustomSignUpEditTextBinding
@@ -22,19 +24,34 @@ class CustomSignUpEditText @JvmOverloads constructor(
 
     init {
         context.withStyledAttributes(attrs, R.styleable.SignUpEditText) {
-            binding.etCustomSignUp.setText(getString(R.styleable.SignUpEditText_inputText))
-            binding.etCustomSignUp.hint = getString(R.styleable.SignUpEditText_hintText)
-            binding.tvCustomSignUp.text = getString(R.styleable.SignUpEditText_rightText)
-            binding.tvCustomSignUp.setTextColor(
-                getColor(
-                    R.styleable.SignUpEditText_rightTextColor,
-                    Color.BLACK
+            binding.etCustomSignUp.apply {
+                filters =
+                    arrayOf<InputFilter>(
+                        LengthFilter(
+                            getInt(
+                                R.styleable.SignUpEditText_maxLen,
+                                10
+                            )
+                        )
+                    )
+                inputType =
+                    getInt(R.styleable.SignUpEditText_android_inputType, InputType.TYPE_CLASS_TEXT)
+                setText(getString(R.styleable.SignUpEditText_inputText))
+                hint = getString(R.styleable.SignUpEditText_hintText)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    getDrawable(R.styleable.SignUpEditText_iconImg),
+                    null, null, null
                 )
-            )
-            binding.etCustomSignUp.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                getDrawable(R.styleable.SignUpEditText_iconImg),
-                null, null, null
-            )
+            }
+            binding.tvCustomSignUp.apply {
+                text = getString(R.styleable.SignUpEditText_rightText)
+                setTextColor(
+                    getColor(
+                        R.styleable.SignUpEditText_rightTextColor,
+                        Color.BLACK
+                    )
+                )
+            }
         }
     }
 
