22ecca54d322bb50c0f312dd8802ca48efa80c0f
박해민 <phm9368@sookmyung.ac.kr>
2021-09-13 20:36:10 +0900

    [Feat] Implement hide keyboard

--- app/src/main/java/com/fitsionary/momspt/presentation/signup/view/SignUpFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/signup/view/SignUpFragment.kt
@@ -1,13 +1,18 @@
 package com.fitsionary.momspt.presentation.signup.view
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.FragmentSignUpBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
+import com.fitsionary.momspt.presentation.custom.CustomDatePickerDialog
 import com.fitsionary.momspt.presentation.signup.viewmodel.SignUpViewModel
+import timber.log.Timber
+
 
 class SignUpFragment :
     BaseFragment<FragmentSignUpBinding, SignUpViewModel>(R.layout.fragment_sign_up) {
@@ -28,9 +33,31 @@ class SignUpFragment :
             viewModel.nicknameValidationCheck()
         }
 
+        binding.tvBirthDay.setOnClickListener {
+            val dialog = CustomDatePickerDialog.CustomDatePickerDialogBuilder()
+                .setOnOkClickedListener {
+                    viewModel.birthDay.value = "2020-12-31"
+                }
+                .create()
+            dialog.show(parentFragmentManager, dialog.tag)
+        }
+
+        binding.containerSignUp.setOnTouchListener { _, _ ->
+            hideKeyboard(activity)
+            true
+        }
+
         // 닉네임 수정 시 다시 유효성 검사 하도록
         viewModel.nickname.observe(viewLifecycleOwner, {
             viewModel.validationResultText.value = ""
         })
+    }
+
+    private fun hideKeyboard(activity: Activity?) {
+        activity?.run {
+            val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            //Find the currently focused view, so we can grab the correct window token from it.
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
