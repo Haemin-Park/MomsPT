4f4daefb1ab330d85a986b4e71f3b9b4c527a771
박해민 <phm9368@sookmyung.ac.kr>
2021-09-27 15:42:29 +0900

    [Feat] Implement navigation to day statistics detail fragment

--- app/src/main/java/com/fitsionary/momspt/presentation/daily/view/MonthStatisticsFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/daily/view/MonthStatisticsFragment.kt
@@ -1,10 +1,14 @@
 package com.fitsionary.momspt.presentation.daily.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.FragmentMonthStatisticsBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
 import com.fitsionary.momspt.presentation.daily.viewmodel.MonthStatisticsViewModel
+
 
 class MonthStatisticsFragment :
     BaseFragment<FragmentMonthStatisticsBinding, MonthStatisticsViewModel>(
@@ -12,5 +16,13 @@ class MonthStatisticsFragment :
     ) {
     override val viewModel: MonthStatisticsViewModel by lazy {
         ViewModelProvider(this).get(MonthStatisticsViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.calendarMonth.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            findNavController().navigate(DailyFragmentDirections.actionMainDailyToDayStatisticsDetailFragment())
+        }
     }
 }
