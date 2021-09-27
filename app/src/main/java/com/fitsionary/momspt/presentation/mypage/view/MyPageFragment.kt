64b486e83d9ba61bc1c481b4ff26405868c2f429
박해민 <phm9368@sookmyung.ac.kr>
2021-09-27 14:07:32 +0900

    [Feat] Implement navigation to edit profile fragment

--- app/src/main/java/com/fitsionary/momspt/presentation/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/mypage/view/MyPageFragment.kt
@@ -3,6 +3,7 @@ package com.fitsionary.momspt.presentation.mypage.view
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.FragmentMypageBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
@@ -21,5 +22,9 @@ class MyPageFragment :
             binding.ivProfile,
             "https://www.dementianews.co.kr/news/photo/201902/1501_1270_5524.jpg"
         )
+
+        binding.btnEditProfile.setOnClickListener {
+            findNavController().navigate(MyPageFragmentDirections.actionMainMypageToEditProfileFragment())
+        }
     }
 }
