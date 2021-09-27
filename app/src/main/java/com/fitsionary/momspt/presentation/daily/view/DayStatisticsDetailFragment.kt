091703569a34553f81924354e21a0501100fabd5
박해민 <phm9368@sookmyung.ac.kr>
2021-09-27 15:41:27 +0900

    [Feat] Implement day statistics detail fragment

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/daily/view/DayStatisticsDetailFragment.kt
@@ -0,0 +1,34 @@
+package com.fitsionary.momspt.presentation.daily.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.fitsionary.momspt.BR
+import com.fitsionary.momspt.R
+import com.fitsionary.momspt.data.model.WorkoutModel
+import com.fitsionary.momspt.databinding.FragmentDayStatisticsDetailBinding
+import com.fitsionary.momspt.databinding.FragmentWorkoutBinding
+import com.fitsionary.momspt.presentation.base.BaseFragment
+import com.fitsionary.momspt.presentation.base.BaseRecyclerViewAdapter
+import com.fitsionary.momspt.presentation.daily.viewmodel.DayStatisticsDetailViewModel
+
+class DayStatisticsDetailFragment :
+    BaseFragment<FragmentDayStatisticsDetailBinding, DayStatisticsDetailViewModel>(R.layout.fragment_day_statistics_detail) {
+    override val viewModel: DayStatisticsDetailViewModel by lazy {
+        ViewModelProvider(this).get(DayStatisticsDetailViewModel::class.java)
+    }
+
+    private val routineAdapter =
+        object : BaseRecyclerViewAdapter<FragmentWorkoutBinding, WorkoutModel>(
+            layoutResId = R.layout.item_workout_large,
+            bindingVariableItemId = BR.LargeWorkoutItem
+        ) {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            vm = viewModel
+            rvDayDetailWorkout.adapter = routineAdapter
+        }
+    }
+}
---
+++ app/src/main/java/com/fitsionary/momspt/presentation/daily/viewmodel/DayStatisticsDetailViewModel.kt
@@ -0,0 +1,17 @@
+package com.fitsionary.momspt.presentation.daily.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.fitsionary.momspt.data.model.WorkoutModel
+import com.fitsionary.momspt.data.model.getTestData
+import com.fitsionary.momspt.presentation.base.BaseViewModel
+
+class DayStatisticsDetailViewModel : BaseViewModel() {
+    private val _workoutList = MutableLiveData<List<WorkoutModel>>()
+    val workoutList: LiveData<List<WorkoutModel>>
+        get() = _workoutList
+
+    init {
+        _workoutList.value = getTestData()
+    }
+}
