edf6624840d44b90101d195789885923f1991e19
박해민 <phm9368@sookmyung.ac.kr>
2021-09-27 13:29:11 +0900

    [Feat] Implement custom month statistics box

---
+++ app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomMonthStatisticsBox.kt
@@ -0,0 +1,31 @@
+package com.fitsionary.momspt.presentation.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import androidx.databinding.DataBindingUtil
+import com.fitsionary.momspt.R
+import com.fitsionary.momspt.databinding.CustomMonthStatisticsBoxBinding
+
+class CustomMonthStatisticsBox @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding: CustomMonthStatisticsBoxBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.custom_month_statistics_box, this, true
+    )
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.MonthStatisticsBox) {
+            binding.tvCustomStatisticsBoxHeader.text =
+                getString(R.styleable.MonthStatisticsBox_headerText)
+            binding.tvCustomStatisticsBoxBody.text =
+                getString(R.styleable.MonthStatisticsBox_bodyText)
+            binding.tvCustomStatisticsBoxFooter.text =
+                getString(R.styleable.MonthStatisticsBox_footerText)
+        }
+    }
+}
