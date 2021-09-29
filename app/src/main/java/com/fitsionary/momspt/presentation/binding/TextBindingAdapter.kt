6569e9061ad507da3e88e015f7855f09a2246153
박해민 <phm9368@sookmyung.ac.kr>
2021-09-29 16:43:45 +0900

    [Feat] Remove unused code

--- app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
@@ -1,8 +1,5 @@
 package com.fitsionary.momspt.presentation.binding
 
-import android.annotation.SuppressLint
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
@@ -15,46 +12,4 @@ fun setValidationResultTextColor(textView: TextView, success: Boolean) {
     } else {
         textView.setTextColor(ResourcesCompat.getColor(textView.resources, R.color.pink, null))
     }
-}
-
-@BindingAdapter("score_gradient")
-fun setScoreGradient(textView: TextView, score: Int) {
-    val paint = textView.paint
-    val width = paint.measureText(textView.text.toString())
-
-    when (score) {
-        in 0..59 -> {
-            textView.setTextColor(textView.resources.getColor(R.color.gradient_blue))
-            textView.paint.shader = null
-        }
-        in 60..89 -> {
-            textView.setTextColor(textView.resources.getColor(R.color.gradient_pink))
-            textView.paint.shader = null
-        }
-        else -> {
-            val textShader = LinearGradient(
-                0f, 0f, width, textView.textSize, intArrayOf(
-                    textView.resources.getColor(R.color.gradient_pink),
-                    textView.resources.getColor(R.color.gradient_blue),
-                ), null, Shader.TileMode.CLAMP
-            )
-            textView.paint.shader = textShader
-        }
-    }
-}
-
-@BindingAdapter("score_comment")
-fun setScoreComment(textView: TextView, score: Int) {
-    val comment = when (score) {
-        in 0..59 -> {
-            "Bad"
-        }
-        in 60..89 -> {
-            "Great!"
-        }
-        else -> {
-            "Perfect!"
-        }
-    }
-    textView.text = comment
 }
