afd60d20812edec1e3aa4e0d8d2ae5ef71b679c5
박해민 <phm9368@sookmyung.ac.kr>
2021-09-29 14:23:04 +0900

    [Feat] Make Parcelable

--- app/src/main/java/com/fitsionary/momspt/data/api/response/Landmark.kt
+++ app/src/main/java/com/fitsionary/momspt/data/api/response/Landmark.kt
@@ -1,9 +1,15 @@
 package com.fitsionary.momspt.data.api.response
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@SuppressLint("ParcelCreator")
+@Parcelize
 data class Landmark(
     val part: String,
     val x: Double,
     val y: Double,
     val z: Double,
     val visibility: Double
-)
+) : Parcelable
--- app/src/main/java/com/fitsionary/momspt/domain/WorkoutLandmarkDomainModel.kt
+++ app/src/main/java/com/fitsionary/momspt/domain/WorkoutLandmarkDomainModel.kt
@@ -1,12 +1,18 @@
 package com.fitsionary.momspt.domain
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
 import com.fitsionary.momspt.data.api.response.Landmark
+import kotlinx.android.parcel.Parcelize
 
+@SuppressLint("ParcelCreator")
+@Parcelize
 data class WorkoutLandmarkDomainModel(
     val poseData: List<PostDataItem>
-) {
+) : Parcelable {
+    @Parcelize
     data class PostDataItem(
         val frame: Long,
         val landmarks: List<Landmark>
-    )
+    ) : Parcelable
 }
