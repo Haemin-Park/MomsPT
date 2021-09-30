ef043dfb6183120f5347db26f7554fa539ce8028
박해민 <phm9368@sookmyung.ac.kr>
2021-09-30 14:05:10 +0900

    [Feat] Implement rank ui

--- app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
@@ -4,6 +4,7 @@ import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import com.fitsionary.momspt.R
+import com.fitsionary.momspt.util.*
 
 @BindingAdapter("validationResultTextColor")
 fun setValidationResultTextColor(textView: TextView, success: Boolean) {
@@ -13,3 +14,46 @@ fun setValidationResultTextColor(textView: TextView, success: Boolean) {
         textView.setTextColor(ResourcesCompat.getColor(textView.resources, R.color.pink, null))
     }
 }
+
+@BindingAdapter("rankText")
+fun setRankText(textView: TextView, rank: String) {
+    when (rank) {
+        A_PLUS -> textView.setTextColor(
+            ResourcesCompat.getColor(
+                textView.resources,
+                R.color.pink,
+                null
+            )
+        )
+        A -> textView.setTextColor(
+            ResourcesCompat.getColor(
+                textView.resources,
+                R.color.pink_d44b62,
+                null
+            )
+        )
+        B -> textView.setTextColor(
+            ResourcesCompat.getColor(
+                textView.resources,
+                R.color.blue_2b9bfd,
+                null
+            )
+        )
+        C -> textView.setTextColor(
+            ResourcesCompat.getColor(
+                textView.resources,
+                R.color.orange_ec7653,
+                null
+            )
+        )
+        D -> textView.setTextColor(
+            ResourcesCompat.getColor(
+                textView.resources,
+                R.color.dark_gray_818181,
+                null
+            )
+        )
+    }
+    textView.text = rank
+}
+
