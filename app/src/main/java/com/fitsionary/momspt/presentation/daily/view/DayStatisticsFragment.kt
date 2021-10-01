c7bf294eb228a1dfd9675939c7106ec488bc551b
박해민 <phm9368@sookmyung.ac.kr>
2021-10-01 13:33:56 +0900

    [Feat] Implement body analysis scenario in daily fragment

--- app/src/main/java/com/fitsionary/momspt/presentation/daily/view/DayStatisticsFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/daily/view/DayStatisticsFragment.kt
@@ -1,6 +1,9 @@
 package com.fitsionary.momspt.presentation.daily.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.FragmentDayStatisticsBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
@@ -10,5 +13,13 @@ class DayStatisticsFragment :
     BaseFragment<FragmentDayStatisticsBinding, DayStatisticsViewModel>(R.layout.fragment_day_statistics) {
     override val viewModel: DayStatisticsViewModel by lazy {
         ViewModelProvider(this).get(DayStatisticsViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvAnalysis.setOnClickListener {
+            findNavController().navigate(DailyFragmentDirections.actionMainDailyToAnalysisFragment())
+        }
     }
 }
