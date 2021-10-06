6cf5fbcb976571f8c0218867b79644a2fb2d4087
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 17:14:32 +0900

    [Feat] Fix rank text binding adapter

--- app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/binding/TextBindingAdapter.kt
@@ -18,42 +18,66 @@ fun setValidationResultTextColor(textView: TextView, success: Boolean) {
 @BindingAdapter("rankText")
 fun setRankText(textView: TextView, rank: String) {
     when (rank) {
-        A_PLUS -> textView.setTextColor(
-            ResourcesCompat.getColor(
-                textView.resources,
-                R.color.pink,
-                null
+        A_PLUS -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.pink,
+                    null
+                )
             )
-        )
-        A -> textView.setTextColor(
-            ResourcesCompat.getColor(
-                textView.resources,
-                R.color.pink_d44b62,
-                null
+            textView.text = "A+"
+        }
+        A -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.pink_d44b62,
+                    null
+                )
             )
-        )
-        B -> textView.setTextColor(
-            ResourcesCompat.getColor(
-                textView.resources,
-                R.color.blue_2b9bfd,
-                null
+            textView.text = "A"
+        }
+        B -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.blue_2b9bfd,
+                    null
+                )
             )
-        )
-        C -> textView.setTextColor(
-            ResourcesCompat.getColor(
-                textView.resources,
-                R.color.orange_ec7653,
-                null
+            textView.text = "B"
+        }
+        C -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.orange_ec7653,
+                    null
+                )
             )
-        )
-        D -> textView.setTextColor(
-            ResourcesCompat.getColor(
-                textView.resources,
-                R.color.dark_gray_818181,
-                null
+            textView.text = "C"
+        }
+        D -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.dark_gray_818181,
+                    null
+                )
             )
-        )
+            textView.text = "D"
+        }
+        NONE -> {
+            textView.setTextColor(
+                ResourcesCompat.getColor(
+                    textView.resources,
+                    R.color.medium_gray,
+                    null
+                )
+            )
+            textView.text = "-"
+        }
     }
-    textView.text = rank
 }
 
