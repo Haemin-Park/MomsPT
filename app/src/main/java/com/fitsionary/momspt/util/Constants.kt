b6377d22d251b845dc7006ce2e90b1e9a7d64e0d
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 16:31:29 +0900

    [Chore] Remove unused code

--- app/src/main/java/com/fitsionary/momspt/util/Constants.kt
+++ app/src/main/java/com/fitsionary/momspt/util/Constants.kt
@@ -1,10 +1,9 @@
 package com.fitsionary.momspt.util
 
-const val TEST_USER_NAME = "fit"
-const val BASE_URL1 = "http://3.37.163.248"
-const val BASE_URL2 = "http://125.129.117.140:4500"
-const val A_PLUS = "A+"
+const val BASE_URL = "http://3.37.163.248"
+const val A_PLUS = "A_PLUS"
 const val A = "A"
 const val B = "B"
 const val C = "C"
 const val D = "D"
+const val NONE = "NONE"
