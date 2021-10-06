95cb2bcd4f47c19d0bdae2db7bec688d34e08e0b
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 17:14:03 +0900

    [Feat] Implement round image binding adapter

--- app/src/main/java/com/fitsionary/momspt/presentation/binding/ImageBindingAdapter.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/binding/ImageBindingAdapter.kt
@@ -3,12 +3,23 @@ package com.fitsionary.momspt.presentation.binding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.fitsionary.momspt.R
 
 @BindingAdapter("image_url")
 fun setImageFromImageUrl(imageView: ImageView, imageUrl: String) {
     Glide.with(imageView.context)
         .load(imageUrl)
+        .placeholder(R.drawable.placeholder)
+        .into(imageView)
+}
+
+@BindingAdapter("round_image_url")
+fun setRoundImageFromImageUrl(imageView: ImageView, imageUrl: String) {
+    Glide.with(imageView.context)
+        .load(imageUrl)
+        .transform(CenterCrop(), RoundedCorners(60))
         .placeholder(R.drawable.placeholder)
         .into(imageView)
 }
