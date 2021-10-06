4325310643db0b2952248c12c12d72f1549e1030
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 16:39:14 +0900

    [Feat] Implement token data store

---
+++ app/src/main/java/com/fitsionary/momspt/datastore/TokenDataStore.kt
@@ -0,0 +1,33 @@
+package com.fitsionary.momspt.datastore
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+
+class TokenDataStore(val context: Context) {
+    private val Context.dataStore by preferencesDataStore("token")
+    private val tokenKey = stringPreferencesKey("token")
+
+    val token: Flow<String> = context.dataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map { preferences ->
+        preferences[tokenKey] ?: ""
+    }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[tokenKey] = token
+        }
+    }
+}
