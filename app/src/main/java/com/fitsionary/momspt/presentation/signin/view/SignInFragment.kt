60a0bfc5014d489104f260f2cd4822e1dec0a8d3
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 16:32:42 +0900

    [Feat] Implement sign in

--- app/src/main/java/com/fitsionary/momspt/presentation/signin/view/SignInFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/signin/view/SignInFragment.kt
@@ -1,34 +1,41 @@
 package com.fitsionary.momspt.presentation.signin.view
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.fitsionary.momspt.MomsPTApplication
 import com.fitsionary.momspt.R
 import com.fitsionary.momspt.databinding.FragmentSignInBinding
 import com.fitsionary.momspt.presentation.base.BaseFragment
-import com.fitsionary.momspt.presentation.intro.view.IntroActivity
+import com.fitsionary.momspt.presentation.main.view.MainActivity
 import com.fitsionary.momspt.presentation.signin.viewmodel.SignInViewModel
+import com.fitsionary.momspt.util.CurrentUser
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import timber.log.Timber
+
 
 class SignInFragment :
     BaseFragment<FragmentSignInBinding, SignInViewModel>(R.layout.fragment_sign_in) {
     override val viewModel: SignInViewModel by lazy {
         ViewModelProvider(this).get(SignInViewModel::class.java)
     }
-    private lateinit var currentActivity: IntroActivity
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is IntroActivity)
-            currentActivity = activity as IntroActivity
-    }
+    var userNickname = ""
+    var userToken = ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            MomsPTApplication.getInstance().getTokenDataStore().token.collect {
+                Timber.i("토큰 $it")
+                CurrentUser.token = it
+            }
+        }
 
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
@@ -37,20 +44,16 @@ class SignInFragment :
                 Timber.i("로그인 성공 $token")
                 UserApiClient.instance.me { user, _ ->
                     if (user != null) {
-                        Timber.i("로그인 유저 $user")
-                        val nickname = user.kakaoAccount?.profile?.nickname
-                        findNavController().navigate(
-                            SignInFragmentDirections.actionSignInFragmentToSignUpFragment(
-                                nickname ?: ""
-                            )
-                        )
+                        viewModel.signIn(user.id)
+                        userToken = token.accessToken
+                        userNickname = user.kakaoAccount?.profile?.nickname ?: ""
                     }
                 }
             }
         }
 
         binding.btnKakaoSignIn.setOnClickListener {
-            currentActivity.apply {
+            requireActivity().apply {
                 if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
                     UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
                 } else {
@@ -58,5 +61,21 @@ class SignInFragment :
                 }
             }
         }
+
+        viewModel.result.observe(viewLifecycleOwner, {
+            it.getContentIfNotHandled()?.let { loginResult ->
+                if (loginResult) {
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        MomsPTApplication.getInstance().getTokenDataStore().saveToken(userToken)
+                    }
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    findNavController().navigate(
+                        SignInFragmentDirections.actionSignInFragmentToSignUpFragment(userNickname)
+                    )
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/fitsionary/momspt/presentation/signin/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/signin/viewmodel/SignInViewModel.kt
@@ -1,6 +1,27 @@
 package com.fitsionary.momspt.presentation.signin.viewmodel
 
-import com.fitsionary.momspt.presentation.base.BaseViewModel
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.fitsionary.momspt.data.api.request.SignInRequest
+import com.fitsionary.momspt.network.NetworkService
+import com.fitsionary.momspt.presentation.base.BaseAndroidViewModel
+import com.fitsionary.momspt.util.Event
+import timber.log.Timber
 
-class SignInViewModel : BaseViewModel() {
+class SignInViewModel(application: Application) : BaseAndroidViewModel(application) {
+    private val _result = MutableLiveData<Event<Boolean>>()
+    val result: LiveData<Event<Boolean>>
+        get() = _result
+
+    fun signIn(id: Long) {
+        NetworkService.api.signIn(SignInRequest(id))
+            .subscribe({
+                Timber.i("성공 $it")
+                _result.postValue(Event(true))
+            }, {
+                Timber.i("실패 $it")
+                _result.postValue(Event(false))
+            })
+    }
 }
