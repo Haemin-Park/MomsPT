7c0e40f5c05f0e7abc65da10d7c1a2b15d7d111e
박해민 <phm9368@sookmyung.ac.kr>
2021-10-06 16:40:22 +0900

    [Feat] Use token data store

--- app/src/main/java/com/fitsionary/momspt/MomsPTApplication.kt
+++ app/src/main/java/com/fitsionary/momspt/MomsPTApplication.kt
@@ -1,14 +1,26 @@
 package com.fitsionary.momspt
 
 import android.app.Application
+import com.fitsionary.momspt.datastore.TokenDataStore
 import com.kakao.sdk.common.KakaoSdk
 import timber.log.Timber
 
+
 class MomsPTApplication : Application() {
+    private lateinit var tokenDataStore: TokenDataStore
+
     override fun onCreate() {
         super.onCreate()
-
+        application = this
         Timber.plant(Timber.DebugTree())
         KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+        tokenDataStore = TokenDataStore(this)
+    }
+
+    fun getTokenDataStore() = tokenDataStore
+
+    companion object {
+        private lateinit var application: MomsPTApplication
+        fun getInstance(): MomsPTApplication = application
     }
 }
