f2d07c5fe5fa8456e461d02c6829785e9a693e6f
박해민 <phm9368@sookmyung.ac.kr>
2021-10-09 14:31:40 +0900

    [Feat] Implement custom back navigation

--- app/src/main/java/com/fitsionary/momspt/presentation/workout/view/WorkoutFragment.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/workout/view/WorkoutFragment.kt
@@ -2,6 +2,7 @@ package com.fitsionary.momspt.presentation.workout.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.fitsionary.momspt.BR
@@ -43,6 +44,17 @@ class WorkoutFragment :
                 holder.binding.rvWorkoutLargeType.bindItems(currentList[position].type)
             }
         }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().finish()
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
