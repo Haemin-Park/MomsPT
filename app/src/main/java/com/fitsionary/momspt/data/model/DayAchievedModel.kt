5f9fc137cb3a090487ee022b01a3ff225c4843fd
박해민 <phm9368@sookmyung.ac.kr>
2021-10-10 14:00:05 +0900

    [Feat] Remove test code

--- app/src/main/java/com/fitsionary/momspt/data/model/DayAchievedModel.kt
+++ app/src/main/java/com/fitsionary/momspt/data/model/DayAchievedModel.kt
@@ -1,10 +1,8 @@
 package com.fitsionary.momspt.data.model
 
-import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@SuppressLint("ParcelCreator")
 @Parcelize
 data class DayAchievedModel(
     val day: String,
@@ -12,16 +10,4 @@ data class DayAchievedModel(
     val entireCount: Int,
     val isSuccess: Boolean
 ) : Parcelable
-
-fun getTestDayAchievedData() =
-    listOf(
-        DayAchievedModel("월", 0, 3, false),
-        DayAchievedModel("화", 3, 3, true),
-        DayAchievedModel("수", 1, 3, false),
-        DayAchievedModel("목", 1, 1, true),
-        DayAchievedModel("금", 1, 1, true),
-        DayAchievedModel("토", 1, 1, true),
-        DayAchievedModel("일", 1, 1, true)
-    )
-
 
