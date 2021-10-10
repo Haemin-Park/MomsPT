c6c6ec8594139143f6722772826d50878ad99a58
박해민 <phm9368@sookmyung.ac.kr>
2021-10-10 23:26:42 +0900

    [Fix] Fix custom seekbar ui

--- app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomSeekbar.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/custom/CustomSeekbar.kt
@@ -26,39 +26,37 @@ class CustomSeekBar @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        val thumbX = (progress.toFloat() / max * width.toFloat())
-        currentText = "${progress}단계 3일차"
-        when (progress) {
-            0 -> currentText = "start!"
-            max -> currentText = "success!"
-        }
-        currentText?.let {
-            textPaint.getTextBounds(it, 0, it.length, textBounds)
-        }
         val drawable = ResourcesCompat.getDrawable(resources, R.drawable.flag, null)
         drawable?.let {
             it.setBounds(
-                width - 125,
-                (height / 4 + textBounds.exactCenterY() - textBounds.height() - 5).toInt(),
+                width - height / 2 - 10,
+                0,
                 width,
-                (height / 4 + textBounds.exactCenterY() + 95).toInt()
+                height / 2 + 10
             )
             it.draw(canvas)
         }
-        val paint = Paint()
-        paint.color = ResourcesCompat.getColor(resources, R.color.pink_99ec5363, null)
-        val rect = RectF(
-            thumbX - textBounds.exactCenterX() - 10,
-            height / 4 + textBounds.exactCenterY() - textBounds.height() - 5,
-            thumbX + textBounds.exactCenterX() + 10,
-            height / 4 + textBounds.exactCenterY() + 10
-        )
-        canvas.drawRoundRect(rect, 30F, 30F, paint)
-        canvas.drawText(
-            currentText!!,
-            thumbX,
-            height / 4 + textBounds.exactCenterY(),
-            textPaint
-        )
+        if (progress in 1 until max) {
+            val thumbX = (progress.toFloat() / max * width.toFloat())
+            currentText = "${progress}단계 3일차"
+            currentText?.let {
+                textPaint.getTextBounds(it, 0, it.length, textBounds)
+            }
+            val paint = Paint()
+            paint.color = ResourcesCompat.getColor(resources, R.color.pink_99ec5363, null)
+            val rect = RectF(
+                thumbX - textBounds.exactCenterX() - 10,
+                height / 4 + textBounds.exactCenterY() - textBounds.height() - 5,
+                thumbX + textBounds.exactCenterX() + 10,
+                height / 4 + textBounds.exactCenterY() + 10
+            )
+            canvas.drawRoundRect(rect, 30F, 30F, paint)
+            canvas.drawText(
+                currentText!!,
+                thumbX,
+                height / 4 + textBounds.exactCenterY(),
+                textPaint
+            )
+        }
     }
 }
