b41fbea36f4f4b99a8940f057bc66afa635601d3
박해민 <phm9368@sookmyung.ac.kr>
2021-10-14 19:23:12 +0900

    [Chore] Remove unnecessary import

--- app/src/main/java/com/fitsionary/momspt/data/api/response/WorkoutListResponse.kt
+++ app/src/main/java/com/fitsionary/momspt/data/api/response/WorkoutListResponse.kt
@@ -1,7 +1,4 @@
 package com.fitsionary.momspt.data.api.response
 
 
-import kotlinx.android.parcel.Parcelize
-import android.os.Parcelable
-
 class WorkoutListResponse : ArrayList<WorkoutListResponseItem>()
