d3298501c0ac68fa66c362a60a151d2b733cc073
박해민 <phm9368@sookmyung.ac.kr>
2021-10-27 14:49:41 +0900

    [Feat] Implement workout counting scenario

--- app/src/main/java/com/fitsionary/momspt/presentation/workoutplay/viewmodel/WorkoutPlayViewModel.kt
+++ app/src/main/java/com/fitsionary/momspt/presentation/workoutplay/viewmodel/WorkoutPlayViewModel.kt
@@ -4,20 +4,26 @@ import android.app.Application
 import android.text.format.DateUtils
 import android.view.View
 import androidx.lifecycle.*
+import com.fitsionary.momspt.R
+import com.fitsionary.momspt.data.enum.WorkoutAnalysisTypeEnum
 import com.fitsionary.momspt.database.getDatabase
 import com.fitsionary.momspt.domain.WorkoutLandmarkDomainModel
 import com.fitsionary.momspt.presentation.base.BaseAndroidViewModel
 import com.fitsionary.momspt.repository.WorkoutPoseLandmarkRepository
 import io.reactivex.rxjava3.subjects.BehaviorSubject
-import timber.log.Timber
 import java.util.*
 
-class WorkoutPlayViewModel(application: Application, workoutCode: String) :
+class WorkoutPlayViewModel(
+    application: Application,
+    workoutCode: String,
+    workoutAnalysisType: WorkoutAnalysisTypeEnum
+) :
     BaseAndroidViewModel(application) {
     private val database = getDatabase(application)
     private val workoutPoseLandmarkRepository =
         WorkoutPoseLandmarkRepository(database)
-    val isGuide: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+
+    val isAiGuide: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
 
     val workoutLandmarks: LiveData<WorkoutLandmarkDomainModel> =
         workoutPoseLandmarkRepository.workoutLandmarks(workoutCode)
@@ -26,61 +32,65 @@ class WorkoutPlayViewModel(application: Application, workoutCode: String) :
     val score: LiveData<Int>
         get() = _score
 
-    private val _cumulativeScore = MutableLiveData<Double>()
-    val cumulativeScore: LiveData<Double>
+    private val _cumulativeScore = MutableLiveData<Int>()
+    val cumulativeScore: LiveData<Int>
         get() = _cumulativeScore
+
+    val currentScore = Transformations.map(score) {
+        when (workoutAnalysisType) {
+            WorkoutAnalysisTypeEnum.SCORING -> application.getString(R.string.score_format, it)
+            WorkoutAnalysisTypeEnum.COUNTING -> application.getString(R.string.count_format, it)
+        }
+    }
+
+    val workoutAnalysisType: String = workoutAnalysisType.name
 
     private lateinit var timer: Timer
 
     private val _timerCountDown = MutableLiveData<Long>()
-    private val _guideCountDown = MutableLiveData<Long>()
-
-    private val _isWorkoutEnd = MutableLiveData<Boolean>(false)
-    val isWorkoutEnd: LiveData<Boolean>
-        get() = _isWorkoutEnd
+    private val _aiGuideCountDown = MutableLiveData<Long>()
 
     val cnt = MutableLiveData<Int>()
 
     val formattedTimer = Transformations.map(_timerCountDown) { time ->
-        if (time <= 0L) {
-            countDownTimerStop()
-            _isWorkoutEnd.value = true
-        }
         DateUtils.formatElapsedTime(time / 1000)
     }
 
-    private val _guideStart = MutableLiveData(false)
-    val guideVisible = Transformations.map(_guideStart) {
-        if (it == null || it)
+    private val _aiGuideStart = MutableLiveData(false)
+    val aiGuideStart: LiveData<Boolean>
+        get() = _aiGuideStart
+
+    val aiGuideVisible = Transformations.map(_aiGuideStart) {
+        if (it == null || it) {
             View.VISIBLE
-        else
+        } else
             View.INVISIBLE
     }
 
     init {
         _score.value = 0
-        _cumulativeScore.value = 0.0
+        _cumulativeScore.value = 0
         cnt.value = 0
     }
 
     fun timerSet(total: Long, guideTime: Long) {
         _timerCountDown.value = total
-        _guideCountDown.value = guideTime
+        _aiGuideCountDown.value = guideTime
     }
 
     fun countDownTimerStart() {
         timer = Timer()
         timer.schedule(object : TimerTask() {
             override fun run() {
-                if (_guideCountDown.value == 0L) {
-                    isGuide.onNext(true)
+                if (_aiGuideCountDown.value == 0L) {
+                    isAiGuide.onNext(true)
                 }
-                if (_guideCountDown.value == -1000L) {
-                    isGuide.onNext(false)
-                    _guideStart.postValue(true)
+                if (_aiGuideCountDown.value == -1000L) {
+                    isAiGuide.onNext(false)
+                    _aiGuideStart.postValue(true)
                 }
                 _timerCountDown.postValue(_timerCountDown.value?.minus(1000))
-                _guideCountDown.postValue(_guideCountDown.value?.minus(1000))
+                _aiGuideCountDown.postValue(_aiGuideCountDown.value?.minus(1000))
             }
         }, 1000, 1000)
     }
@@ -90,18 +100,24 @@ class WorkoutPlayViewModel(application: Application, workoutCode: String) :
     }
 
     fun setScore(score: Int) {
-        if (_guideStart.value == true && _timerCountDown.value != 0L) {
+        if (_aiGuideStart.value == true && _timerCountDown.value != 0L) {
             _score.postValue(score)
-            _cumulativeScore.postValue(_cumulativeScore.value?.plus(score))
-            cnt.postValue(cnt.value?.plus(1))
+            if (workoutAnalysisType == WorkoutAnalysisTypeEnum.SCORING.name) {
+                _cumulativeScore.postValue(_cumulativeScore.value?.plus(score))
+                cnt.postValue(cnt.value?.plus(1))
+            }
         }
     }
 
-    class ViewModelFactory(private val application: Application, private val workoutCode: String) :
+    class ViewModelFactory(
+        private val application: Application,
+        private val workoutCode: String,
+        private val workoutType: WorkoutAnalysisTypeEnum
+    ) :
         ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             return if (modelClass.isAssignableFrom(WorkoutPlayViewModel::class.java)) {
-                WorkoutPlayViewModel(application, workoutCode) as T
+                WorkoutPlayViewModel(application, workoutCode, workoutType) as T
             } else {
                 throw IllegalArgumentException()
             }
